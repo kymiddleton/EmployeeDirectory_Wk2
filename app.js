7d120f68c85c5352c239f71af445c5c32bedb7a1
Choleslaw <41842273+kymiddleton@users.noreply.github.com>
2018-09-10 10:36:52 -0400

    Added DOM file and updated CSS

--- app.js
+++ app.js
@@ -2,47 +2,47 @@ const employeeList = [
   {
     name: 'Jan',
     officeNum: 1,
-    phoneNum: '222-222-2222'
+    phoneNum: '(222) 222-2222'
   },
   {
     name: 'Juan',
     officeNum: 304,
-    phoneNum: '489-789-8789'
+    phoneNum: '(489) 789-8789'
   },
   {
     name: 'Margie',
     officeNum: 789,
-    phoneNum: '789-789-7897'
+    phoneNum: '(789) 789-7897'
   },
   {
     name: 'Sara',
     officeNum: 32,
-    phoneNum: '222-789-4654'
+    phoneNum: '(222) 789-4654'
   },
   {
     name: 'Tyrell',
     officeNum: 3,
-    phoneNum: '566-621-0452'
+    phoneNum: '(566) 621-0452'
   },
   {
     name: 'Tasha',
     officeNum: 213,
-    phoneNum: '789-766-5675'
+    phoneNum: '(789) 766-5675'
   },
   {
     name: 'Ty',
     officeNum: 211,
-    phoneNum: '789-766-7865'
+    phoneNum: '(789) 766-7865'
   },
   {
     name: 'Sarah',
     officeNum: 345,
-    phoneNum: '222-789-5231'
+    phoneNum: '(222) 789-5231'
   }
 ];
 
 const printEmployee = function (i) {
-  render("name: " + employeeList[i].name + " / office: " + employeeList[i].officeNum + " / phone: " + employeeList[i].phoneNum);
+  render("Name: " + employeeList[i].name + " / Office: " + employeeList[i].officeNum + " / Phone: " + employeeList[i].phoneNum);
   return true;
 }
 
